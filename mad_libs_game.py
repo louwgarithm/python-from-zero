0c8abf196e0f2f4b19dd4780584ceec7680561ef
louwgarithm <louwgarithm@gmail.com>
2026-07-10 16:15:21 +0200

    Fix print statement for plural noun output

---
+++ mad_libs_game.py
@@ -0,0 +1,7 @@
+colour = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + colour)
+print(plural_noun + " are blue")
+print("I like " + celebrity)
