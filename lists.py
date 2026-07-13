6f9cc3dfee54e76626f2ca4b6bdbb944cfb43b56
louwgarithm <louwgarithm@gmail.com>
2026-07-13 10:08:17 +0200

    Kdrama list and print statements

---
+++ lists.py
@@ -0,0 +1,8 @@
+# Lists
+kdrama = ["My Absolute Boyfriend", "Hotel Del Luna", "Goblin", "I am Not A Robot"]
+
+print(kdrama)
+print(kdrama[0])
+print(kdrama[1:])
+print(kdrama[-1])
+print(kdrama[1:3])
