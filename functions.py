e10a3d371b89d371d3cb6209473783ca1c2b17d7
louwgarithm <louwgarithm@gmail.com>
2026-07-13 10:10:14 +0200

    User_details function to display user info

---
+++ functions.py
@@ -0,0 +1,7 @@
+# Functions
+
+def user_details(user_name, email):
+    print("Your username is: " + str(user_name))
+    print("Your email is: " + str(email))
+
+user_details("don1", "don1@gmail.com")
