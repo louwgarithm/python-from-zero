36b930b0b874bffd28c8907e1b13f47820ed4353
louwgarithm <louwgarithm@gmail.com>
2026-07-15 14:04:12 +0200

    Fix typo in kdrama_list entry

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+kdrama_list = {
+    1: "Goblin",
+    2: "Hotel Del Luna",
+    3: "I Am Not A Robot",
+    4: "Memories Of The Alhambra",
+    5: "9 Times Time Travel"
+}
+
+print(kdrama_list[1])
+print(kdrama_list.get(2))
+print(kdrama_list.get(6))
